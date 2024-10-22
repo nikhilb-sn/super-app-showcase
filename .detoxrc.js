0c03cb1358387d6ec9034e2a6c314a7b64b5605f
Nikhil Batra <nikhil.x.batra@goindigo.in>
2024-10-22 15:55:42 +0530

    Adding files for detox Android

--- .detoxrc.js
+++ .detoxrc.js
@@ -22,8 +22,8 @@ module.exports = {
     },
     'android.debug': {
       type: 'android.apk',
-      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
-      build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
+      binaryPath: 'packages/host/android/app/build/outputs/apk/debug/app-debug.apk',
+      build: 'cd packages/host/android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
       reversePorts: [
         8081
       ]
@@ -31,7 +31,7 @@ module.exports = {
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
+      build: 'cd packages/host/android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
     }
   },
   devices: {
@@ -50,7 +50,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a_API_30_x86'
+        avdName: 'Medium_Phone_API_35'
       }
     }
   },
